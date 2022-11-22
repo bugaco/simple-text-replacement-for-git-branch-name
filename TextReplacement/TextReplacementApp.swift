dc773951e51920ac8c5cfa46f6751ec6b0df1a6a
李懿哲 <li_yizhe@icloud.com>
2022-11-22 13:30:37 +0800

    Initial Commit

---
+++ TextReplacement/TextReplacementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextReplacementApp.swift
+//  TextReplacement
+//
+//  Created by BugaCo on 2022/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TextReplacementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
